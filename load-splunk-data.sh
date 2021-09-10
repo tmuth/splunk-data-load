1e618a07ca6c060e112e3d3799544b46a8147953
Tyler Muth <tmuth@splunk.com>
2021-09-10 16:24:08 -0400

    Added token auth

--- load-splunk-data.sh
+++ load-splunk-data.sh
@@ -19,15 +19,20 @@ the data to load. sample.cfg will be generated if it doesn't already exist.
 }
 
 # URL escape codes used to pass special characters. DO NOT CHANGE!
-dqt="%22"
-pct="%25"
+dqt="%22" # double quote
+pct="%25" # percent sign
+dol="%24" # dollar sign
+slb="%5C" # backslash
 
 function gen_sample_cfg {
   echo "
 SPLUNK_HOST=localhost:8089
+AUTH_TOKEN=# Token authentication is the preferred method over username/password and is documented here:
+# https://docs.splunk.com/Documentation/Splunk/latest/Security/EnableTokenAuth
 SPLUNK_USERNAME=admin
 SPLUNK_PASS=welcome1
 INDEX=sample_index
+INDEX_TYPE=event
 SOURCETYPE=sample_sourcetype
 DIRECTORY=.
 #DIRECTORY=/Volumes/GoogleDrive/My\ Drive/Projects/splunking-json/Docker/data/fio
@@ -51,6 +56,7 @@ function gen_settings {
 #
 DEBUG_TIMESTAMP=F # T or F
 DEBUG=T # T or F 
+SHOW_BTOOL=F # F or F 
 SHOW_GREAT_8=T # T or F
 SHOW_WALKLEX=F # T or F
 SHOW_EVENT_SUMMARY=T # T or F
@@ -119,31 +125,48 @@ if [[ -z "$SPLUNK_HOME" ]];then
 fi
 # exit 0
 
+
+CLI_AUTH_OPTION=()
+AUTH_OPTION=()
+if [ ! -z "$AUTH_TOKEN" ]; then
+    CLI_AUTH_OPTION+=( -token ${AUTH_TOKEN} )
+    AUTH_OPTION+=( -H "Authorization: Bearer ${AUTH_TOKEN}" )
+    #echo "Using Token Auth"
+else
+    CLI_AUTH_OPTION+=( -auth "${SPLUNK_USERNAME}:${SPLUNK_PASS}" )
+    AUTH_OPTION+=( -u ${SPLUNK_USERNAME}:${SPLUNK_PASS} )
+    #echo "Using User:Pass Auth"
+fi
+
+
 function splunk_search {
   #echo ${1}
-  curl -s -k -u ${SPLUNK_USERNAME}:${SPLUNK_PASS}  \
+  curl -s -k "${AUTH_OPTION[@]}" \
     -X POST https://${SPLUNK_HOST}/services/search/jobs/ \
     -d search="${1}" \
     -d exec_mode=oneshot -d output_mode=csv -d adhoc_search_level=verbose
 }
 
 function splunk_search_polling {
-  SEARCH_LEVEL=${2:-verbose} 
-  RESULTS=`curl -s -k -u ${SPLUNK_USERNAME}:${SPLUNK_PASS}  \
-    -X POST https://${SPLUNK_HOST}/services/search/jobs \
-    -d search="${1}" \
-    -d adhoc_search_level=${SEARCH_LEVEL}`
+    SEARCH_LEVEL=${2:-verbose} 
+    curl_opts_common=( -s -k -d adhoc_search_level=${SEARCH_LEVEL} )
+    
+    curl_opts=( "${curl_opts_common[@]}"  "${AUTH_OPTION[@]}" -d search="${1}" )
+    #echo ${curl_opts[@]}
 
-    #echo "$RESULTS"
+    RESULTS=`curl "${curl_opts[@]}"  -X POST https://${SPLUNK_HOST}/services/search/jobs`
+ 
     SID=`echo $RESULTS | sed -e 's,.*<sid>\([^<]*\)<\/sid>.*,\1,g' ` 
+    printf "\n"
     echo "SID: $SID"
 
     SEARCH_STATUS=""
-    counter=10
+    counter=30 # will check for status=DONE this many times, every ${wait_seconds}
+    wait_seconds=1
     while [ $counter -gt 0 ]
     do
-        OUTPUT=`curl -s -k -u ${SPLUNK_USERNAME}:${SPLUNK_PASS}  \
-            -X GET https://${SPLUNK_HOST}/services/search/jobs/${SID}  `
+        curl_opts=( "${curl_opts_common[@]}"  "${AUTH_OPTION[@]}"  )
+        OUTPUT=`curl "${curl_opts[@]}" -X POST https://${SPLUNK_HOST}/services/search/jobs/${SID}`
         #echo "$OUTPUT"
         STATUS=`echo $OUTPUT | sed -e 's,.*<s:key name=\"dispatchState\">\([^<]*\)<\/s\:key>.*,\1,g' `
         #echo "$STATUS"
@@ -153,33 +176,32 @@ function splunk_search_polling {
             break 1
         fi
         counter=$(( $counter - 1 ))
-        sleep 1
+        sleep ${wait_seconds}
     done
 
     if [[ "$SEARCH_STATUS" = "DONE" ]]; then
-        #echo "Getting Search Results"
-        SEARCH_RESULTS=`curl -s -k -u ${SPLUNK_USERNAME}:${SPLUNK_PASS}  \
-            -X GET https://${SPLUNK_HOST}/services/search/jobs/${SID}/results \
-            -d output_mode=csv `
-        echo "$SEARCH_RESULTS" | sed 's/,/ ,/g' | column -t -s, 
+        curl_opts=( "${curl_opts_common[@]}"  "${AUTH_OPTION[@]}" -d output_mode=csv  )
+        SEARCH_RESULTS=`curl "${curl_opts[@]}" -X GET https://${SPLUNK_HOST}/services/search/jobs/${SID}/results`
+        echo "$SEARCH_RESULTS" | sed 's/,/ ,/g' | column -t -s, \
+        | awk 'NR == 1 {print $0;print $0}; NR > 1 {print $0}' \
+        | sed '2 s/[^[:space:]]/-/g'
     fi
 }
 
 function config_reload {
   local CONFIG="${1}"
-
-  curl_opts=( --silent --output /dev/null -k -u ${SPLUNK_USERNAME}:${SPLUNK_PASS})
+  curl_opts=( --silent --output /dev/null -k "${AUTH_OPTION[@]}" )
   
   if should_show_section "SHOW_INDEX_CONF"; then
     curl_opts+=( --write-out "${CONFIG} reload, http-status: %{http_code}\n" )
   fi
 
-  curl "${curl_opts[@]}"  -X POST https://${SPLUNK_HOST}/services/configs/${CONFIG}/_reload
+  curl "${curl_opts[@]}"  -X POST https://${SPLUNK_HOST}/servicesNS/-/-/admin/${CONFIG}/_reload
 }
 
 should_show_section "SHOW_INDEX_CONF"  && print_section "INDEX_CONF_BEGIN"
 
-curl_opts=( --silent --output /dev/null -k -u ${SPLUNK_USERNAME}:${SPLUNK_PASS})
+curl_opts=( --silent --output /dev/null -k "${AUTH_OPTION[@]}")
 should_show_section "SHOW_INDEX_CONF"  && curl_opts+=( --write-out "delete index http-status: %{http_code}\n" )
 
 # delete the index
@@ -189,15 +211,18 @@ curl "${curl_opts[@]}" \
 if should_show_section "SHOW_INDEX_CONF"; then
   curl_opts+=( --write-out "create index http-status: %{http_code}\n" )
 fi
+
+INDEX_DATA_TYPE=${INDEX_TYPE:-event} 
 curl  "${curl_opts[@]}" \
   https://${SPLUNK_HOST}/servicesNS/nobody/${APP_NAME}/data/indexes  \
     -d name=${INDEX} \
-    -d datatype=event
+    -d datatype=${INDEX_DATA_TYPE}
 
 #printf "\n"
 config_reload "conf-inputs"
 config_reload "conf-fields"
 config_reload "conf-transforms"
+config_reload "transforms-reload"
 config_reload "conf-props"
 printf "\n"
 should_show_section "SHOW_INDEX_CONF"  && print_section "INDEX_CONF_END"
@@ -244,15 +269,17 @@ do
       echo "host segment: $HOST_SEGMENT_COMPUTED"
     fi
 
-    ${SPLUNK_HOME}/bin/./splunk add oneshot -source "$i" -index ${INDEX} -sourcetype ${SOURCETYPE} ${HOST_SEGMENT_COMPUTED} -auth "${SPLUNK_USERNAME}:${SPLUNK_PASS}";
+    #echo ${CLI_AUTH_OPTION[@]}
+    ${SPLUNK_HOME}/bin/./splunk add oneshot -source "$i" -index ${INDEX} -sourcetype ${SOURCETYPE} ${HOST_SEGMENT_COMPUTED} ${CLI_AUTH_OPTION[@]};
+
     if [ "${DEBUG}" == "T" ];then
       echo "DEBUG waiting a few seconds so errors will be logged"
       sleep 3
       printf "\n\nErrors:\n"
-      splunk_search "search index=_* OR index=* log_level=ERROR sourcetype=splunkd earliest=-1m 
+      splunk_search_polling "search index=_* OR index=* log_level=ERROR sourcetype=splunkd earliest=-1m 
         | where LIKE(data_source,${dqt}${pct}${i}${dqt}) 
         | eval time=strftime(_time, ${dqt}${pct}I:${pct}M:${pct}S:${pct}p${dqt})
-        | table time,event_message" | sed 's/\"//g'| sed 's/,/ ,/g' | column -c 2 -t -s,
+        | table time,event_message" 
       printf "\n\n"
     fi
   fi
@@ -260,7 +287,7 @@ done
 print_section "ONESHOT_END"
 
 
-if [ "${DEBUG}" == "T" ];then
+if [ "${SHOW_BTOOL}" == "T" ];then
   print_section "BTOOL_BEGIN"
   echo "btool check for errors in sourcetype:"
   ${SPLUNK_HOME}/bin/./splunk btool check | grep ${SOURCETYPE}
@@ -274,30 +301,33 @@ echo "Waiting a few seconds so some of the files will be indexed..."
 sleep 3
 
 if should_show_section "SHOW_EVENT_SUMMARY"; then
-  print_section "EVENT_SUMMARY_BEGIN"
-  printf "\n\nEvent Count:"
-  splunk_search "search index=${INDEX} sourcetype=${SOURCETYPE} | stats count"
-  printf "\n\nField Summary:\n"
-  splunk_search "search index=${INDEX} sourcetype=${SOURCETYPE} | fieldsummary | fields field,count" \
-    | sed 's/,/ ,/g' | column -t -s,
-  print_section "EVENT_SUMMARY_END"
+  if [ "${INDEX_DATA_TYPE}" == "event" ];then
+    print_section "EVENT_SUMMARY_BEGIN"
+    printf "\n\nEvent Count:"
+    splunk_search_polling "search index=${INDEX} sourcetype=${SOURCETYPE} | stats count"
+    printf "\n\nField Summary:\n"
+    splunk_search_polling "search index=${INDEX} sourcetype=${SOURCETYPE} | fieldsummary | fields field,count" 
+    print_section "EVENT_SUMMARY_END"
+  fi
 fi 
 
 if should_show_section "SHOW_WALKLEX"; then
-  print_section "WALKLEX_BEGIN"
-  echo "Rolling hot buckets to warm for index ${INDEX}"
-  OUTPUT=`${SPLUNK_HOME}/bin/./splunk _internal call /data/indexes/${INDEX}/roll-hot-buckets -auth "${SPLUNK_USERNAME}:${SPLUNK_PASS}" | grep HTTP`
-  echo "${OUTPUT}"
-  printf "\n"
+  if [ "${INDEX_DATA_TYPE}" == "event" ];then
+    print_section "WALKLEX_BEGIN"
+    echo "Rolling hot buckets to warm for index ${INDEX}"
+    OUTPUT=`${SPLUNK_HOME}/bin/./splunk _internal call /data/indexes/${INDEX}/roll-hot-buckets ${CLI_AUTH_OPTION[@]} | grep HTTP`
+    echo "${OUTPUT}"
+    printf "\n"
 
 
-  SEARCH_STRING=" |  walklex index=${dqt}${INDEX}${dqt} type=field | search NOT field=${dqt} *${dqt}  "
-  SEARCH_STRING+="| where  NOT LIKE(field,${dqt}date_${pct}${dqt}) "
-  SEARCH_STRING+="| search NOT field IN (${dqt}source${dqt},${dqt}sourcetype${dqt},${dqt}punct${dqt},${dqt}linecount${dqt},${dqt}timeendpos${dqt},${dqt}timestartpos${dqt},${dqt}_indextime${dqt},${dqt}snc_io_parser${dqt}) "
-  SEARCH_STRING+="| stats sum(distinct_values) by field"
-  #echo "$SEARCH_STRING"
-  splunk_search_polling "${SEARCH_STRING}"
-  print_section "WALKLEX_END"
+    SEARCH_STRING=" |  walklex index=${dqt}${INDEX}${dqt} type=field | search NOT field=${dqt} *${dqt}  "
+    SEARCH_STRING+="| where  NOT LIKE(field,${dqt}date_${pct}${dqt}) "
+    SEARCH_STRING+="| search NOT field IN (${dqt}source${dqt},${dqt}sourcetype${dqt},${dqt}punct${dqt},${dqt}linecount${dqt},${dqt}timeendpos${dqt},${dqt}timestartpos${dqt},${dqt}_indextime${dqt},${dqt}snc_io_parser${dqt}) "
+    SEARCH_STRING+="| stats sum(distinct_values) by field"
+    #echo "$SEARCH_STRING"
+    splunk_search_polling "${SEARCH_STRING}"
+    print_section "WALKLEX_END"
+  fi
 fi
 
 
@@ -312,9 +342,35 @@ if [ "${DEBUG_TIMESTAMP}" == "T" ];then
   print_section "DEBUG_TIMESTAMP_END"
 fi
 
-print_section "EVENT_SEARCH_BEGIN"
-echo "search index=${INDEX} sourcetype=${SOURCETYPE} earliest=-5y | sort - _time | head 20 "
-splunk_search_polling "search index=${INDEX} sourcetype=${SOURCETYPE} | sort - _time | head 20 | fields - _raw,index,timestamp,eventtype,punct,splunk_server,splunk_server_group,_bkt,_cd,tag,_sourcetype,_si,_indextime,source,_eventtype_color,linecount,${dqt}tag::eventtype${dqt},date,date_hour,date_mday,date_minute,date_month,date_second,date_wday,date_year,date_zone,_kv,_serial | fields ${REPORT_FIELDS} | table *" 
-print_section "EVENT_SEARCH_END"
-printf "\n\n"
+if [ "${INDEX_DATA_TYPE}" == "event" ];then
+  print_section "EVENT_SEARCH_BEGIN"
+  echo "search index=${INDEX} sourcetype=${SOURCETYPE} earliest=-5y | sort - _time | head 20 "
+  splunk_search_polling "search index=${INDEX} sourcetype=${SOURCETYPE} | sort - _time | head 20 | fields - _raw,index,timestamp,eventtype,punct,splunk_server,splunk_server_group,_bkt,_cd,tag,_sourcetype,_si,_indextime,source,_eventtype_color,linecount,${dqt}tag::eventtype${dqt},date,date_hour,date_mday,date_minute,date_month,date_second,date_wday,date_year,date_zone,_kv,_serial | fields ${REPORT_FIELDS} | table *" 
+  print_section "EVENT_SEARCH_END"
+  printf "\n\n"
+else
+  print_section "METRIC_SUMMARY_BEGIN"
+  printf "\n\nMetric Names:\n"
+  SEARCH_STRING=" | mcatalog values(metric_name) as metric_name  WHERE index=${INDEX} | mvexpand metric_name | table metric_name "
+  echo "${SEARCH_STRING}"
+  splunk_search_polling "${SEARCH_STRING}"
 
+  printf "\n\nMetric Dimensions:\n"
+  SEARCH_STRING=" | mcatalog values(_dims) AS dimensions  WHERE index=${INDEX} | mvexpand dimensions | table dimensions "
+  echo "${SEARCH_STRING}"
+  splunk_search_polling "${SEARCH_STRING}"
+
+  printf "\n\nMetric Value Summary by Metric Name:\n"
+
+  SEARCH_STRING=" | mcatalog values(metric_name) as metric_name WHERE index=drive_metrics "
+  SEARCH_STRING+="| mvexpand metric_name "
+  SEARCH_STRING+="| map search=${dqt} | mstats avg(_value) as avg_value prestats=false WHERE metric_name=${slb}${dqt}${dol}metric_name${dol}${slb}${dqt} AND index=${slb}${dqt}drive_metrics${slb}${dqt} by metric_name span=1m ${dqt} "
+  SEARCH_STRING+="| stats avg(avg_value) as avg_value,min(_time) as min_time,max(_time) as max_time by metric_name "
+  SEARCH_STRING+="| eval min_time=strftime(min_time,${dqt}${pct}m/${pct}d/${pct}y ${pct}H:${pct}M${dqt}),max_time=strftime(max_time,${dqt}${pct}m/${pct}d/${pct}y ${pct}H:${pct}M${dqt}),avg_value=round(avg_value,1) "
+  SEARCH_STRING+="| table metric_name,avg_value,min_time,max_time "
+  echo "${SEARCH_STRING}"
+  splunk_search_polling "${SEARCH_STRING}"
+
+  print_section "METRIC_SUMMARY_END"
+  printf "\n\n"
+fi
